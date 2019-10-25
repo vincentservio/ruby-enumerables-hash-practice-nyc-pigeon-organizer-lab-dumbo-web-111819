3fbece3a5c30cc5a56ef11152a995a86bdc92d18
vincent servio <vincentservio@gmail.com>
2019-10-25 00:13:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list = {}
+  data.each do |color_gender_lives, value|
+  value.each do |stats, all_names|
+  all_names.each do |name|
+   if list[name] == nil 
+      list[name] = {}
+end
+  if list[name][color_gender_lives] == nil 
+    list[name][color_gender_lives] = []
+  end 
+ list[name][color_gender_lives].push(stats.to_s)
+end 
+end 
+end 
+list
 end
